303f74b4507097c9292ad9e8be8f75d13644937f
OlszokKrzysztof <116442554+OlszokKrzysztof@users.noreply.github.com>
2022-11-13 14:55:37 +0100

    Zadanie6a

---
+++ Zadanie6/6a/config.php
@@ -0,0 +1,7 @@
+<?php
+$conf->root_path = dirname(__FILE__);
+$conf->server_name = 'localhost:80';
+$conf->server_url = 'http://'.$conf->server_name;
+$conf->app_root = '/AS/Zadanie6/6a/';
+$conf->app_url = $conf->server_url.$conf->app_root;
+?>
---
+++ Zadanie6/6a/init.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'app/helper/Config.php';
+$conf = new Config();
+include dirname(__FILE__).'/config.php'; //ustaw konfigurację
+
+function &getConf(){ global $conf; return $conf; }
+
+function &getMessages(){ global $msgs; return $msgs; }
+
+//przygotuj Twig, twórz tylko raz - wtedy kiedy potrzeba
+$twig = null;	
+function &getTwig(){
+	global $twig;
+	if (!isset($twig)){
+        require_once getConf()->root_path.'/lib/Twig/Autoloader.php';
+        
+        Twig_Autoloader::register();
+        //załaduj szablony (wskazanie folderów z potrzebnymi szablonami)
+        $loader = new Twig_Loader_Filesystem(getConf()->root_path.'/templates'); //szablon ogólny
+        $loader->addPath(getConf()->root_path.'/app/view'); //szablon strony kalkulatora
+        //skonfiguruj folder cache
+        $twig = new Twig_Environment($loader, array(
+            'cache' => getConf()->root_path.'/twig_cache',
+        ));
+	}
+	return $twig;
+}
+require_once getConf()->root_path.'/app/core/functions.php';
+
+$action = getFromRequest('action');
