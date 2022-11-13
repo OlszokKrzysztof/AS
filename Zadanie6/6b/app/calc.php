c950aea8939bed4520ab33e93f5a58d6ecce554e
OlszokKrzysztof <116442554+OlszokKrzysztof@users.noreply.github.com>
2022-11-13 15:19:00 +0100

    Zadanie6b

---
+++ Zadanie6/6b/app/calc.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app;
+use app\helper\Config;
+
+// KONTROLER strony kalkulatora
+//require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+//załaduj Twig
+
